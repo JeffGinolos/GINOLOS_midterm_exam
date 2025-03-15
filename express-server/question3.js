d58edcf68043b1860d6d43137d4ae7d053a4e968
unknown <YY520@ccs.local>
2025-03-15 14:24:22 +0800

    Add Express server for /test route

---
+++ express-server/question3.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/test', (req, res) => {
+    res.json({ message: "Express is working! [JEFF-L GINOLOS]" });
+});
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
